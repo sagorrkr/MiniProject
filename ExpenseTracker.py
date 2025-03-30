e3cca26657d37097862fb4b2e8e42d09cb111d83
sagorrkr <ridoykum9@gmail.com>
2025-03-30 17:11:12 +0800

    Expense Tracker

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -7,3 +7,13 @@ class Expense:
         
     def __str__(self):
         return f" {self.date} - {self.category.capitalize()}: {self.amount:.2f} "
+
+class ExpenseTracker:
+    def __init__(self, budget_limit = None):
+        self.expenses = []
+        self.budget_limit = budget_limit
+
+    def add_expense(self, amount, category, date = None):
+        expense = Expense(amount, category, date)
+        self.expense.append(expense)
+        print(f"Added Expense: {expense}")
