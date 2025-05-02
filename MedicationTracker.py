6ee1f4d084386b01f23b2299bdcee69174867780
sagorrkr <ridoykum9@gmail.com>
2025-05-02 14:25:22 +0800

    Personal medication schedule management

--- MedicationTracker.py
+++ MedicationTracker.py
@@ -1,4 +1,12 @@
 #managing medication schedules
-
+from datetime import datetime, timedelta
 class Medication:
-    def __init__(self, dosage, frequencym stock):
+    def __init__(self, name,  dosage, frequency, stock):
+        self.name = name
+        self.dosage = dosage 
+        self.frequency = frequency.lower()
+        self.stock = int(stock)
+
+
+
+
